b6842f8db1621a013e8446022272cd546e42d52b
Tanner Gill <contacttannergill@gmail.com>
2023-12-20 19:20:12 -0800

    Initial Commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Tanner on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
